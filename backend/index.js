64bf0480c6c5efe641355245a1f26ddabd20dea9
Sujal Chauhan <chauhansujal1107@gmail.com>
2024-10-16 13:33:57 +0530

    deployment change 2

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin : '*',
+    origin : process.env.FRONTEND,
     credentials : true,
 }
 app.use(cors(corsOptions));
